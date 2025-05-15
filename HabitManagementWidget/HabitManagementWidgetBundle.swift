fb08b4539b8f5d1342aa3f618a2248ab82024baf
kangddong <rkdehddud96@naver.com>
2025-05-15 12:50:44 +0900

    <add> Widget 추가

---
+++ HabitManagementWidget/HabitManagementWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  HabitManagementWidgetBundle.swift
+//  HabitManagementWidget
+//
+//  Created by 강동영 on 5/15/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HabitManagementWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        HabitManagementWidget()
+    }
+}
