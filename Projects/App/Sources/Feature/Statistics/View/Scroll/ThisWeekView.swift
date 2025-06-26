88292a3a14be6583f81ecb4a22c1701570a6bf32
kangddong <rkdehddud96@naver.com>
2025-06-27 00:08:41 +0900

    review: 리뷰 반영

--- Projects/App/Sources/Feature/Statistics/View/Scroll/ThisWeekView.swift
+++ Projects/App/Sources/Feature/Statistics/View/Scroll/ThisWeekView.swift
@@ -5,36 +5,39 @@
 //  Created by 한수진 on 5/25/25.
 //
 
-import SwiftUI
 import ComposableArchitecture
+import SwiftUI
 
 struct ThisWeekView: View {
     private let store: StoreOf<StatisticsFeature>
-    
+
     private var ratioSpacing: CGFloat
     private var frame_size: CGFloat
     private var getColor: (String) -> Color
-    
-    init(store: StoreOf<StatisticsFeature>, ratioSpacing: CGFloat, frame_size: CGFloat, getColor: @escaping (String) -> Color) {
+
+    init(
+        store: StoreOf<StatisticsFeature>, ratioSpacing: CGFloat, frame_size: CGFloat,
+        getColor: @escaping (String) -> Color
+    ) {
         self.store = store
         self.ratioSpacing = ratioSpacing
         self.frame_size = frame_size
         self.getColor = getColor
     }
-    
+
     var body: some View {
         WithViewStore(store, observe: { $0 }) { viewStore in
             VStack(alignment: .center, spacing: ratioSpacing) {
-                Text("Test")
-//                ForEach(Array(viewStore.staticsData.thisWeek.enumerated()), id:\.offset) { index, date in
-//                    Text("\(date)")
-//                        .scaledFrame(width: frame_size, height: frame_size)
-//                        .overlay(
-//                            RoundedRectangle(cornerRadius: ratioSpacing, style: .continuous)
-//                                .fill(date == "" ? HabitColor.defaultGreen.color : getColor(date))
-//                                .scaledFrame(width: frame_size, height: frame_size)
-//                        )
-//                }
+                ForEach(Array(viewStore.staticsData.thisWeek.enumerated()), id: \.offset) {
+                    index, date in
+                    Text("\(date)")
+                        .scaledFrame(width: frame_size, height: frame_size)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: ratioSpacing, style: .continuous)
+                                .fill(date == "" ? HabitColor.defaultGreen.color : getColor(date))
+                                .scaledFrame(width: frame_size, height: frame_size)
+                        )
+                }
             }
         }
     }
--- Projects/App/Sources/Habit_ManagementApp.swift
+++ Projects/App/Sources/Habit_ManagementApp.swift
@@ -5,47 +5,53 @@
 //  Created by 한수진 on 2022/03/24.
 //
 
+import ComposableArchitecture
+import FirebaseCore
+import RealmSwift
 import SwiftUI
 import UIKit
-import RealmSwift
-//import PartialSheet
 import UserNotifications
-import FirebaseCore
-import ComposableArchitecture
 
 class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     let gcmMessageIDKey = "gcm.message_id"
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
         print("didfinish")
-        
+
         if #available(iOS 10.0, *) {
             // For iOS 10 display notification (sent via APNS)
             UNUserNotificationCenter.current().delegate = self
-            
+
             let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
             UNUserNotificationCenter.current().requestAuthorization(
                 options: authOptions,
-                completionHandler: {_, _ in })
+                completionHandler: { _, _ in })
         } else {
             let settings: UIUserNotificationSettings =
-            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
             application.registerUserNotificationSettings(settings)
         }
-        
-//        FirebaseApp.configure()
+
+        //        FirebaseApp.configure()
         application.registerForRemoteNotifications()
         return true
     }
-    
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
-                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+
+    func application(
+        _ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+        fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
+    ) {
         print("didreceive")
         print(userInfo)
         completionHandler(UIBackgroundFetchResult.newData)
-        
+
     }
-    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+    func application(
+        _ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?
+    ) -> UIInterfaceOrientationMask {
         // 세로방향 고정
         return UIInterfaceOrientationMask.portrait
     }
@@ -57,18 +63,19 @@ struct Habit_ManagementApp: SwiftUI.App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     private let persistenceController = PersistenceController.shared
     private let notification = NotificationCenterManager()
-    
+
     private let store: StoreOf<AppFeature>
     private let setting: Setting = Setting()
-    
+
     init() {
         self.store = Store(initialState: AppFeature.State(), reducer: { AppFeature() })
         let config = RealmSwift.Realm.Configuration(
-            schemaVersion: 5, // 새로운 스키마 버전 설정
+            schemaVersion: 5,  // 새로운 스키마 버전 설정
             migrationBlock: { migration, oldSchemaVersion in
                 if oldSchemaVersion < 5 {
                     // 1-1. 마이그레이션 수행(버전 2보다 작은 경우 버전 2에 맞게 데이터베이스 수정)
-                    migration.enumerateObjects(ofType: Statistics.className()) { oldObject, newObject in
+                    migration.enumerateObjects(ofType: Statistics.className()) {
+                        oldObject, newObject in
                         newObject!["year"] = 0
                         newObject!["days"] = []
                         newObject!["week"] = []
@@ -79,11 +86,11 @@ struct Habit_ManagementApp: SwiftUI.App {
                 }
             }
         )
-        
+
         // 2. Realm이 새로운 Object를 쓸 수 있도록 설정
         Realm.Configuration.defaultConfiguration = config
     }
-    
+
     var body: some Scene {
         WindowGroup {
             MainView(store: store)
@@ -105,5 +112,3 @@ struct Habit_ManagementApp: SwiftUI.App {
         }
     }
 }
-
-
